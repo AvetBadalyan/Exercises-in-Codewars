f9bf049fd4e20fb7cc4f2fac76d28c61248d73b1
AvetBadalyan <avetbadalyan@gmail.com>
2022-06-20 22:52:43 +0400

    feat: divide people as senior or open

---
+++ 02.seniorOrOpen.js
@@ -0,0 +1,34 @@
+/* The Western Suburbs Croquet Club has two categories of membership, Senior and Open.To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26;
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. 
+Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) 
+stating whether the respective member is to be placed in the senior or open category.
+
+Example
+input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+output = ["Open", "Open", "Senior", "Open", "Open", "Senior"] */
+
+function openOrSenior(data) {
+  let output = data.map((pair) => {
+    if (pair[0] >= 55 && pair[1] > 7) {
+      return "Senior";
+    } else {
+      return "Open";
+    }
+  });
+  return output;
+}
+
+console.log(
+  openOrSenior([
+    [45, 12],
+    [55, 21],
+    [19, -2],
+    [104, 20],
+  ])
+);
+// ['Open', 'Senior', 'Open', 'Senior']
