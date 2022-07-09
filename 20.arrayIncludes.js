2c3ff4c2f6b0dcc5bda297e18dbc7caa96a092b9
AvetBadalyan <avetbadalyaN@gmail.com>
2022-07-09 19:20:50 +0400

    feat: check if array includes the number and remove all occurancies

---
+++ 20.arrayIncludes.js
@@ -0,0 +1,27 @@
+// Array.diff
+
+/* Your goal in this kata is to implement a difference function, which subtracts one list from another and returns the result.
+
+It should remove all values from list a, which are present in list b keeping their order.
+
+arrayDiff([1,2],[1]) == [2]
+If a value is present in b, all of its occurrences must be removed from the other:
+
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+ */
+
+// 1st solution
+function arrayDiff(a, b) {
+  let newArray = [];
+  a.forEach((number) => {
+    if (!b.includes(number)) {
+      newArray.push(number);
+    }
+  });
+  return newArray;
+}
+
+// 2nd solution
+function array_diff(a, b) {
+  return a.filter((e) => !b.includes(e));
+}
